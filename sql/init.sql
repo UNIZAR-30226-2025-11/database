f74655644a540ca97122cc85e774e50cd61e5af2
Dorian Wozniak <dorian.wozniak@gmail.com>
2025-02-23 17:26:06 +0100

    Extended users table

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,13 @@
 CREATE TABLE IF NOT EXISTS users (
+    -- Identity and auth
     id uuid UNIQUE PRIMARY KEY,
-    username varchar(128) UNIQUE,
-    password varchar(128)
+    username varchar(128) UNIQUE NOT NULL,
+    password varchar(128) NOT NULL,
+
+    -- Statistics
+    games_played integer NOT NULL CHECK (games_played >= 0),
+    games_won integer NOT NULL CHECK (games_won >= 0),
+
+    -- Currency available
+    coins integer NOT NULL CHECK (coins >= 0)
 )
